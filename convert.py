f50b6f8ee64e8c23b1fa1d839df29c837043ba88
Максим <51298369+MaksPV@users.noreply.github.com>
2022-01-24 18:47:16 +0300

    Add files via upload

--- convert.py
+++ convert.py
@@ -12,6 +12,7 @@ with open('dostoevsky.csv', encoding="utf-8") as File:
 headers = {'X-API-KEY': 'b49a04fd-a84a-4a14-a02c-ca3dc3320b9f',
            'Content-Type': 'application/json'}
 
+timer = 0
 a = ""
 rzdel = list()
 for i in results:
@@ -43,9 +44,15 @@ for i in results:
 
         description = js["description"]
         film_length = js["filmLength"]
+        if film_length is not None:
+            timer += film_length
+        else:
+            film_length = "-"
         genres = [i["genre"] for i in js["genres"]]
         countries = [i["country"] for i in js["countries"]]
         year = js["year"]
+        if pic == "https://s3.afisha.ru/mediastorage/ba/2e/6388c71dbba74e15919d824b2eba.jpg":
+            pic = js["posterUrl"]
 
         staff = dict()
         for i in js1:
@@ -55,28 +62,33 @@ for i in results:
 
         staff_ = ""
         for k, i in list(staff.items())[:4]:
-            staff_ += k + ": " + ", ".join(i[:4]) + "\n"
+            staff_ += "**" + k + "**: " + ", ".join(i[:4]) + "<br>\n"
 
         if js["nameOriginal"] is None:
             res = f'## {js["nameRu"]}' + "\n\n"
         else:
             res = f'## {js["nameRu"]} / ориг. {js["nameOriginal"]}' + "\n\n"
-        res += f'<img src="{pic}" alt="drawing" width="225" align="right"/>' + "\n\n"
+        if pic:
+            res += f'<img src="{pic}" border="3" alt="drawing" width="225" align="right"/>' + "\n\n"
 
         res += f"**Жанры:** {', '.join(genres)}<br>" + "\n"
         res += f"**Год:** {year}<br>" + "\n"
         res += f"**Страна:** {', '.join(countries)}<br>" + "\n"
         res += f"**Продолжительность:** {film_length} минут<br>" + "\n"
-        if js['ratingKinopoisk'] is not None:
-            res += f"**Кинопоиск:** {js['ratingKinopoisk']}"
-        if js['ratingImdb'] is not None:
-            res += f", **IMDB:** {js['ratingImdb']}"
-        if js['ratingFilmCritics'] is not None:
-            res += f", **Критики:** {js['ratingFilmCritics']}"
+
+        g = {"Кинопоиск": js['ratingKinopoisk'],
+             "IMDB": js['ratingImdb'],
+             "Критики": js['ratingFilmCritics']}
+
+        g_ = list()
+        for k, i in g.items():
+            if i is not None:
+                g_.append(f"**{k}**: {i}")
+        res += ", ".join(g_)
         res += "<br>\n\n"
-        res += f"{description}<br><br>" + "\n"
+        if description is not None:
+            res += f"{description}<br><br>" + "\n"
         res += staff_ + "\n"
-        res += f"<br>"
         res += f"<a href='{url}'><img src='https://github.com/MaksPV/Dostoevsky/raw/main/playbutt.png' border='0'></a>"
         if wiki:
             res += f"<a href='{wiki}'><img src='https://github.com/MaksPV/Dostoevsky/raw/main/wikiread.png' border='0'></a>"
@@ -85,7 +97,8 @@ for i in results:
         res += "\n\n"
     else:
         res = f"## {name}" + "\n\n"
-        res += f'<img src="{pic}" alt="drawing" width="225" align="right"/>' + "\n\n"
+        if pic:
+            res += f'<img src="{pic}" border="3" alt="drawing" width="225" align="right"/>' + "\n\n"
         res += f"**Год:** {year}<br>" + "\n"
         res += f"**Страна:** {country}" + "\n\n"
         res += f"{description}<br>" + "\n"
@@ -109,3 +122,4 @@ a = res + "\n" + a
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(a)
     f.close()
+print(timer)
